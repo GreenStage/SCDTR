ad6343183180e0723e141b6d9f6bd3a040220b4e
Eduardo Gomes <eduardo.l.gomes@tecnico.ulisboa.pt>
2017-11-29 15:35:57 +0000

    makefile

--- server/main.cpp
+++ server/main.cpp
@@ -1,11 +1,13 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include "server.hpp"
 
 int main(){
     try
     {
         boost::asio::io_service io_service;
+        Server * sv = new Server();
     }
     catch(std::exception& e){
         std::cerr << e.what() << std::endl;
